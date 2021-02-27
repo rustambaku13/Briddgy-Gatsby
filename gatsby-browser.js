c5aa1b880ca7bb0afe0923f44f9fd71b678d0f50
rustambaku13 <q.rustam@code.edu.az>
2021-02-27 22:33:20 +0400

    Salams

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,6 +3,7 @@ import myTheme from "./src/theme"
 import { ChakraProvider } from "@chakra-ui/react"
 import "./src/styles/index.scss"
 import "react-modern-calendar-datepicker/lib/DatePicker.css"
+import "swiper/swiper.scss"
 
 export function wrapRootElement({ element }) {
   return <ChakraProvider theme={myTheme}>{element}</ChakraProvider>
